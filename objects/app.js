f3e58041c8a1cf6633374feda6e0f4e47e4e318b
Edgar Gonzalez <edgar_gm6@hotmail.com>
2021-03-10 12:12:22 -0600

    objects

---
+++ objects/app.js
@@ -0,0 +1,18 @@
+let persona = {
+  nombre: "Edgar",
+  apellido: "Gonzalez",
+  getName: function () {
+    return `${this.nombre} ${this.apellido}`;
+  },
+  domicilio: {
+    calle: "Marciano Cabrera #321",
+    colonia: "Tepeyac",
+    cp: 28110,
+    municipio: "Tecoman",
+  },
+  getDom: function () {
+    return `${this.domicilio.calle} Colonia: ${this.domicilio.colonia} CP: ${this.domicilio.cp}, ${this.domicilio.municipio}`;
+  },
+};
+console.log(persona.getName());
+console.log(persona.getDom());
